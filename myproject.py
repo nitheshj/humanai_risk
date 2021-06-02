28c1e18245e2908e847e88edcc86d6adaec428bb
Nithesh Javvaji <nithesh.bharadwaj579@gmail.com>
2021-06-02 14:41:02 -0400

    RMSEs to DB

--- myproject.py
+++ myproject.py
@@ -9,8 +9,10 @@ from datetime import datetime
 import uuid
 # import jwt
 import pandas as pd
-import numpy as np
-from flask import Flask, request, send_from_directory, session
+# import numpy as np
+from flask import Flask, request, send_from_directory
+from sklearn.metrics import mean_squared_error
+# from flask import session
 # from flask_session import Session
 from database import Database
 import configparser
@@ -19,39 +21,20 @@ import random
 import os
 from flask_cors import CORS
 from ml_model import ModelRun
-import pickle
+# import pickle
 
 PATH = os.path.join(os.path.abspath('..'), 'Tier1/client/')
 
 app = Flask(__name__, static_url_path="", static_folder=PATH)
-CORS(app, resources={r"/*": {"origins": "https://studycrafter.com"}}, supports_credentials=True,
-     allow_headers=["Content-Type", "Origin", "X-Requested-With", "Accept", "x-auth"])
-# SESSION_TYPE = 'filesystem'
-# SESSION_PERMANENT = False  # Session cookie will expire when browser is closed
 app.secret_key = 'human_ai'
 app.config.from_object(__name__)
-# Session(app)
+CORS(app, resources={r"/*": {"origins": "https://studycrafter.com"}}, supports_credentials=True,
+     allow_headers=["Content-Type", "Origin", "X-Requested-With", "Accept", "x-auth"])
 
 config = configparser.ConfigParser()
 config.read('config.ini', encoding='utf-8-sig')
 
 USERS = dict()
-
-# To make the session cookie expire after 5 Minutes of closing browser
-# @app.before_request
-# def make_session_permanent():
-#     session.permanent = True
-#     app.permanent_session_lifetime = timedelta(minutes=5)
-
-
-# @app.after_request
-# def after_request(response):
-#     """ after_request """
-#
-#     response.headers.add('Access-Control-Allow-Origin', 'https://studycrafter.com')
-#     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-#     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-#     return response
 
 
 @app.route('/set')
@@ -73,15 +56,37 @@ def start():
     USERS[user]['att1'] = [570, 539, 617, 653, 684, 725, 749, 771, 806, 812]
     USERS[user]['att2'] = [575, 534, 602, 663, 691, 729, 769, 777, 811, 823]
     USERS[user]['user_choice'] = [0] * treatment[treat]
-    # USERS[user]['user_choice'] = np.random.uniform(low=1, high=7, size=(treatment[treat],))
     USERS[user]['two'] = random.sample(range(1, treatment['C']+1), 50)
+    USERS[user]['user_choice_2'] = dict()
+    USERS[user]['reco_actual'] = dict()
 
     return user
 
+def compute_rmse(first_dict:dict,second_dict:dict):
+    first_list = list()
+    second_list = list()
+
+    for each in first_dict.keys():
+        first_list.append(first_dict[each])
+        second_list.append(second_dict[each])
+
+    rmse = mean_squared_error(first_list,second_list,squared=False)
+
+    return rmse
 
 @app.route('/out')
 def out():
     user = str(request.args.get('user_id'))
+
+    test_ideal_rmse = float(compute_rmse(USERS[user]['user_choice_2'],USERS[user]['recommendations']))
+    test_actual_rmse = float(compute_rmse(USERS[user]['user_choice_2'],USERS[user]['reco_actual']))
+    train_rmse = float(USERS[user]['train_rmse'])
+
+    db = Database(config)
+
+    db.store_data("""INSERT INTO chicagofaces.player_model(user, train_rmse,test_actual_rmse,test_ideal_rmse)
+                            VALUES (%s,%s,%s,%s);""",
+                  (user, train_rmse, test_actual_rmse, test_ideal_rmse,))
 
     item = USERS.pop(user, "User Don't exist")
 
@@ -107,6 +112,8 @@ def storechoice():
 
     if phase == 'one':
         USERS[user]['user_choice'][image_id-1] = user_choice
+    elif phase == 'two':
+        USERS[user]['user_choice_2'][image_id] = user_choice
 
     #ToDo- save choices to database-done
     db = Database(config)
@@ -134,8 +141,6 @@ def nextid():
     if phase == 'one' or phase == 'two':
         seq.remove(new_id)
         USERS[user][phase] = seq
-
-    # print(new_choice)
 
     return str(new_id)
 
@@ -168,9 +173,8 @@ def getcredit():
     Retrieve credit rating of an image
     :return:
     """
-    image_id = int(request.args.get('image_id'))
-
     db = Database(config)
+    image_id = int(request.args.get('image_id'))
 
     # T1 =199, T2 = 202, C = 196
     credit = db.query_database("""SELECT test.Credit_rating FROM test  
@@ -201,6 +205,7 @@ def getimage():
     user_id = str(request.args.get('user_id'))
     treat = str(request.args.get('treat', "T1"))
 
+    treatment = str()
     if treat == "T1":
         treatment = "C" if phase == "att1" else treat
     elif treat == "T2":
@@ -208,15 +213,14 @@ def getimage():
     elif treat == "C":
         treatment = "T2" if phase == "att2" else treat
 
-    name = 'init.png'
-
     db = Database(config)
 
     # T1 =199, T2 = 202, C = 196
     results = db.query_database("""SELECT images.image, test.Credit_rating FROM images INNER JOIN test ON images.Target = test.Target 
                                     WHERE TreatID = %s AND Treatment = %s""", (image_id, treatment,))
 
-    write_file(results[0], os.path.join(PATH + user_id + "/", name))
+    name_image = 'init.png'
+    write_file(results[0], os.path.join(PATH + user_id + "/", name_image))
 
     return str(results[1])
 
@@ -241,10 +245,9 @@ def runmodel():
     # treatment = {'T1': 199, 'T2': 202, 'C': 196}
     # data['User_choice'] = np.random.uniform(low=1, high=5, size=(treatment[treat],))
 
-    # data.to_csv('test_model.csv', encoding='utf-8', index=False)
     mod = ModelRun(dataset_csv=data)
     mod.whole_procedure(user)
-    # USERS[user]['rmse1'] = mod.rf_rmse
+    USERS[user]['train_rmse'] = mod.rf_rmse
 
     pred = ModelRun(pd.read_csv('game_face_data_C.csv'))
     USERS[user]['recommendations'] = pred.rec(user)
@@ -259,34 +262,28 @@ def getreco():
     :return:
     """
     image_id = int(request.args.get('image_id'))
-    # image_id = float(input("id please:"))
-    # attention = int(request.args.get('att'))
     user_id = str(request.args.get('user_id'))
-    # user_id = "test"
-    # treat = str(request.args.get('treat', "T1"))
-
-    # loaded_model = pickle.load(open('rf_regs/'+user_id, 'rb'))
-    # X_test = 0
-    # result = loaded_model.predict(X_test)
-    # print(result)
 
     result = USERS[user_id]['recommendations'][image_id]
 
     if 0 <= result <= 1.5:
-        return str(1)
+        result = 1
     elif 1.5 < result <= 2.5:
-        return str(2)
+        result = 2
     elif 2.5 < result <= 3.5:
-        return str(3)
+        result = 3
     elif 3.5 < result <= 4.5:
-        return str(4)
+        result = 4
     elif 4.5 < result <= 5.5:
-        return str(5)
+        result = 5
+
+    USERS[user_id]['reco_actual'][image_id] = result
+
+    return str(result)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # ToDo - done
     app.run(host='0.0.0.0', debug=True, threaded=True)
-    # sess = Session()
-    # sess.init_app(app)
+
